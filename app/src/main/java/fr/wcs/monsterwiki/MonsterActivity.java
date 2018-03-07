931d7491c43c5c1cff71adb38c08cdc94c5eff94
Clairehere <b_claire@hotmail.com>
2018-03-07 21:54:01 +0100

    caracteristique en commentaire

--- app/src/main/java/fr/wcs/monsterwiki/MonsterActivity.java
+++ app/src/main/java/fr/wcs/monsterwiki/MonsterActivity.java
@@ -47,6 +47,13 @@ public class MonsterActivity extends AppCompatActivity {
             Drawable drawableElement1 = ContextCompat.getDrawable(this, R.drawable.bte_fire);
             element1.setImageDrawable(drawableElement1);
 
+            /*
+            puissance=242;
+            life=81,
+            vitesse=192,
+            stamina=100
+            */
+
         }
 
         if (test.equals("2")) {
@@ -55,7 +62,11 @@ public class MonsterActivity extends AppCompatActivity {
             profil.setImageDrawable(drawableProfil1);
             Drawable drawableElement1 = ContextCompat.getDrawable(this, R.drawable.bte_nature);
             element1.setImageDrawable(drawableElement1);
-
+            /*power=209;
+            LIFE=91;
+            Vitesse=220;
+            Stamina=100
+             */
         }
         if (test.equals("3")) {
 
@@ -63,6 +74,11 @@ public class MonsterActivity extends AppCompatActivity {
             profil.setImageDrawable(drawableProfil1);
             Drawable drawableElement1 = ContextCompat.getDrawable(this, R.drawable.bte_earth);
             element1.setImageDrawable(drawableElement1);
+            /*power=192;
+            LIFE=115;
+            Vitesse=192;
+            Stamina=100;
+             */
         }
         if (test.equals("4")) {
 
@@ -70,6 +86,11 @@ public class MonsterActivity extends AppCompatActivity {
             profil.setImageDrawable(drawableProfil1);
             Drawable drawableElement1 = ContextCompat.getDrawable(this, R.drawable.bte_thunder);
             element1.setImageDrawable(drawableElement1);
+            /*power=192;
+            LIFE=81;
+            Vitesse=275;
+            Stamina=100;
+             */
         }
         if (test.equals("5")) {
 
@@ -79,6 +100,11 @@ public class MonsterActivity extends AppCompatActivity {
             element1.setImageDrawable(drawableElement1);
             Drawable drawableElement2 = ContextCompat.getDrawable(this, R.drawable.bte_fire);
             element2.setImageDrawable(drawableElement2);
+            /*power=272;
+            LIFE=89;
+            Vitesse=242;
+            Stamina=110;
+             */
         }
         if (test.equals("6")) {
 
@@ -88,6 +114,11 @@ public class MonsterActivity extends AppCompatActivity {
             element1.setImageDrawable(drawableElement1);
             Drawable drawableElement2 = ContextCompat.getDrawable(this, R.drawable.bte_fire);
             element2.setImageDrawable(drawableElement2);
+            /*power=297;
+            LIFE=123;
+            Vitesse=198;
+            Stamina=120;
+             */
         }
         if (test.equals("7")) {
 
@@ -97,6 +128,11 @@ public class MonsterActivity extends AppCompatActivity {
             element1.setImageDrawable(drawableElement1);
             Drawable drawableElement2 = ContextCompat.getDrawable(this, R.drawable.bte_fire);
             element2.setImageDrawable(drawableElement2);
+            /*power=286;
+            LIFE=118;
+            Vitesse=286;
+            Stamina=130;
+             */
         }
         if (test.equals("8")) {
 
@@ -106,6 +142,11 @@ public class MonsterActivity extends AppCompatActivity {
             element1.setImageDrawable(drawableElement1);
             Drawable drawableElement2 = ContextCompat.getDrawable(this, R.drawable.bte_fire);
             element2.setImageDrawable(drawableElement2);
+            /*power=331;
+            LIFE=135;
+            Vitesse=306;
+            Stamina=140;
+             */
         }
         if (test.equals("9")) {
 
@@ -115,6 +156,14 @@ public class MonsterActivity extends AppCompatActivity {
             element1.setImageDrawable(drawableElement1);
             Drawable drawableElement2 = ContextCompat.getDrawable(this, R.drawable.bte_earth);
             element2.setImageDrawable(drawableElement2);
+            /*power=290;
+            LIFE=104;
+            Vitesse=244;
+            Stamina=120;
+            Exemple
+            String speed2="654";
+            statspeed.setText(speed2);
+             */
         }
 
     }
