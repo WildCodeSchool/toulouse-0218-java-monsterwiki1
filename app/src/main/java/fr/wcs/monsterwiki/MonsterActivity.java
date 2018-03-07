e9bee35345b35c232710b2af73e45e7727b6eb78
Clairehere <b_claire@hotmail.com>
2018-03-07 18:50:33 +0100

    model image

--- app/src/main/java/fr/wcs/monsterwiki/MonsterActivity.java
+++ app/src/main/java/fr/wcs/monsterwiki/MonsterActivity.java
@@ -1,11 +1,18 @@
 package fr.wcs.monsterwiki;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import static java.security.AccessController.getContext;
 
 public class MonsterActivity extends AppCompatActivity {
 
@@ -30,9 +37,17 @@ public class MonsterActivity extends AppCompatActivity {
         String test = intent.getStringExtra("extraplace");
         if (test.equals("0")){
             Toast.makeText(this, "Monstre 0", Toast.LENGTH_SHORT).show();
+
+
+
         }
         if (test.equals("1")){
             Toast.makeText(this, "Monstre 1", Toast.LENGTH_SHORT).show();
+
+            Drawable drawableProfil1=ContextCompat.getDrawable(this,R.drawable.fire_lion_0fire);
+            profil.setImageDrawable(drawableProfil1);
+
+
         }
 
     }
